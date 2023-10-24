4d90562bd1b2127504de02f70730aae95dc47c04
Luiz Antonio Anacleto Zuchinali <luizanacletozuchinali@gmail.com>
2023-10-24 00:25:12 -0300

    camera impl

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,14 @@
 #include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
+#include "Camera.hpp"
 
 #include <memory>
 
 using namespace Graphics;
 
 const int WINDOW_WIDTH = 2560, WINDOW_HEIGHT = 1440;
+auto MainCamera = Camera(glm::vec3(0, 0, -5));
 
 void GLFWWindowDeleter(GLFWwindow *window) {
     glfwDestroyWindow(window);
@@ -59,7 +61,8 @@ std::shared_ptr<GLFWwindow> CreateWindow() {
     ImGuiIO &io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-    io.FontGlobalScale = 1.25f;
+
+    io.FontGlobalScale = 1.50f;
 
     float xscale, yscale;
     glfwGetWindowContentScale(window.get(), &xscale, &yscale);
@@ -69,6 +72,32 @@ std::shared_ptr<GLFWwindow> CreateWindow() {
     ImGui_ImplOpenGL3_Init();
 
     return window;
+}
+
+void HandleInput(const std::shared_ptr<GLFWwindow> &window, Camera &camera, float deltaTime) {
+    auto pKeyPress = glfwGetKey(window.get(), GLFW_KEY_P);
+    if (pKeyPress && glfwGetInputMode(window.get(), GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
+        glfwSetInputMode(window.get(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    else if (pKeyPress && glfwGetInputMode(window.get(), GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
+        glfwSetInputMode(window.get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    if (glfwGetKey(window.get(), GLFW_KEY_ESCAPE)) {
+        GLFWWindowDeleter(window.get());
+        exit(0);
+    }
+
+    if (glfwGetKey(window.get(), GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(ECameraMovement::FORWARD, deltaTime);
+    if (glfwGetKey(window.get(), GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(ECameraMovement::BACKWARD, deltaTime);
+    if (glfwGetKey(window.get(), GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(ECameraMovement::LEFT, deltaTime);
+    if (glfwGetKey(window.get(), GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(ECameraMovement::RIGHT, deltaTime);
+
+    double xPos, yPos;
+    glfwGetCursorPos(window.get(), &xPos, &yPos);
+    camera.ProcessMouseMovement(xPos, yPos);
 }
 
 int main() {
@@ -95,7 +124,6 @@ int main() {
 
     shader.Use();
     shader.SetTexture("tex", brickTex);
-    auto view = glm::translate(glm::mat4(1), glm::vec3(0, 0, -5));
     auto projection = glm::perspective(
             glm::radians(45.0f),
             (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT,
@@ -103,11 +131,9 @@ int main() {
             100.0f
     );
 
-    shader.SetMat4("view", view);
     shader.SetMat4("projection", projection);
 
     glBindVertexArray(0);
-
 
     glm::vec3 cubePositions[] = {
             glm::vec3(0.0f, 0.0f, 0.0f),
@@ -122,28 +148,20 @@ int main() {
             glm::vec3(-1.3f, 1.0f, -1.5f)
     };
 
+    float lastTime = glfwGetTime();
+    float deltaTime;
+
     while (!glfwWindowShouldClose(window.get())) {
+        float currentTime = glfwGetTime();
+        deltaTime = currentTime - lastTime;
+        lastTime = currentTime;
+
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
         ImGui::ShowDemoWindow();
 
-        if (glfwGetKey(window.get(), GLFW_KEY_ESCAPE)) {
-            GLFWWindowDeleter(window.get());
-            exit(0);
-        }
-
-        if (glfwGetKey(window.get(), GLFW_KEY_A)) {
-            glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-        }
-
-        if (glfwGetKey(window.get(), GLFW_KEY_S)) {
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        }
-
-        if (glfwGetKey(window.get(), GLFW_KEY_D)) {
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        }
+        HandleInput(window, MainCamera, deltaTime);
 
         glClearColor(0, 0, 0, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -151,11 +169,13 @@ int main() {
         brickTex.ActivateAndBind();
         shader.Use();
 
+        shader.SetMat4("view", MainCamera.GetViewMatrix());
+
         glBindVertexArray(VAO);
         for (unsigned int i = 0; i < sizeof(cubePositions) / sizeof(glm::vec3); i++) {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            model = glm::rotate(model, (float) glfwGetTime() + (float) i, glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::rotate(model, currentTime + (float) i, glm::vec3(1.0f, 0.3f, 0.5f));
             shader.SetMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
